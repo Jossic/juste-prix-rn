5716b7de4a4518b36cc4079f6c9b67fe26ccef66
Jossic <jossic.lapierre@gmail.com>
2022-01-14 22:27:38 +0100

    Adding 3 first screens + navigation

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AppNavigator from './navigation/AppNavigator';
 
 //Redux
 // import { createStore, combineReducers } from 'redux';
--- navigation/Navigators.js
+++ navigation/Navigators.js
@@ -1,16 +1,39 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Colors from '../constants/Colors';
 import HistoryScreen from '../screens/HistoryScreen';
 import HomeScreen from '../screens/HomeScreen';
 import SettingsScreen from '../screens/SettingsScreen';
+import { Ionicons } from 'react-native-vector-icons';
 
 const Tab = createBottomTabNavigator();
 
-export default function AppTabNavigator() {
+export const AppTabNavigator = () => {
 	return (
-		<Tab.Navigator>
+		<Tab.Navigator
+			screenOptions={({ route }) => ({
+				tabBarIcon: ({ focused, color, size }) => {
+					let iconName;
+					if (route.name === 'Home') {
+						iconName = focused
+							? 'game-controller'
+							: 'game-controller-outline';
+					} else if (route.name === 'Historique') {
+						iconName = focused ? 'beer' : 'beer-outline';
+					} else if (route.name === 'Paramètres') {
+						iconName = focused ? 'settings' : 'settings-outline';
+					}
+					return (
+						<Ionicons name={iconName} size={size} color={color} />
+					);
+				},
+			})}
+			tabBarOptions={{
+				activeTintColor: Colors.primary,
+				// inactiveTintColor: Color.primaryFaded,
+			}}>
 			<Tab.Screen name='Home' component={HomeScreen} />
 			<Tab.Screen name='Historique' component={HistoryScreen} />
 			<Tab.Screen name='Paramètres' component={SettingsScreen} />
 		</Tab.Navigator>
 	);
-}
+};
