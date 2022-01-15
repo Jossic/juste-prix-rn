35af2ed02474811d65188668d1a1de7cf97f571d
Jossic <jossic.lapierre@gmail.com>
2022-01-15 14:33:48 +0100

    Playing screen done

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
 	StyleSheet,
 	Text,
@@ -6,12 +6,15 @@ import {
 	Image,
 	SafeAreaView,
 	Dimensions,
-	TouchableHighlight,
 	TouchableOpacity,
+	TextInput,
 } from 'react-native';
+import { Ionicons } from 'react-native-vector-icons';
 import Colors from '../constants/Colors';
 
 const HomeScreen = () => {
+	const [game, setGame] = useState(false);
+	const [num, setNum] = useState();
 	return (
 		<View style={{ flex: 1, backgroundColor: 'white' }}>
 			<SafeAreaView style={{ flex: 1 }}>
@@ -27,11 +30,42 @@ const HomeScreen = () => {
 						<Text style={styles.orange}>0</Text> et{' '}
 						<Text style={styles.orange}>1000</Text>.
 					</Text>
-					<TouchableOpacity
-						style={styles.button}
-						onPress={() => console.log(`press`)}>
-						<Text style={{ color: 'white' }}>Commencer</Text>
-					</TouchableOpacity>
+					{game ? (
+						// <TouchableOpacity
+						// 	style={styles.button}
+						// 	onPress={() => setGame(!game)}>
+						// 	<Text style={{ color: 'white' }}>Commencer</Text>
+						// </TouchableOpacity>
+						<>
+							<View style={styles.textPrice}>
+								<Text style={{ color: 'white' }}>
+									#1 Quel est le juste prix ?
+								</Text>
+							</View>
+
+							<View style={styles.viewInput}>
+								<TextInput
+									style={styles.textInput}
+									value={num}
+									keyboardType='number-pad'
+									onChangeText={setNum}
+									autoFocus={true}
+								/>
+								<Ionicons
+									style={styles.arrow}
+									name='arrow-forward-outline'
+									size={35}
+									color={Colors.secondary}
+								/>
+							</View>
+						</>
+					) : (
+						<TouchableOpacity
+							style={styles.button}
+							onPress={() => setGame(!game)}>
+							<Text style={{ color: 'white' }}>Commencer</Text>
+						</TouchableOpacity>
+					)}
 				</View>
 			</SafeAreaView>
 		</View>
@@ -48,6 +82,35 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		paddingTop: 20,
 		paddingBottom: 10,
+	},
+	textPrice: {
+		color: 'white',
+		backgroundColor: Colors.ternary,
+		paddingVertical: 12,
+		paddingHorizontal: 8,
+		borderRadius: 5,
+		marginTop: 20,
+	},
+	textInput: {
+		color: 'black',
+		paddingVertical: 9,
+		paddingHorizontal: 5,
+		borderRadius: 5,
+		height: 45,
+		width: Dimensions.get('window').width * 0.25,
+	},
+	viewInput: {
+		marginTop: 20,
+		backgroundColor: Colors.quaternary,
+		flexDirection: 'row',
+		borderColor: 'white',
+		borderBottomColor: Colors.primary,
+		borderStyle: 'solid',
+		borderWidth: 3,
+	},
+	arrow: {
+		paddingRight: 10,
+		alignSelf: 'center',
 	},
 	image: {
 		maxWidth: Dimensions.get('window').width * 0.5,
